fdc48b98f52064e5d1700ac72bd280850c2422a9
Veronica Hernandez <vlhernandez@gmail.com>
2016-02-12 17:04:25 -0600

    Added alert button subview function

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,18 +1,25 @@
-var m              = require('mithril')
+var m              = require('mithril');
 var Layout = module.exports;
 
 Layout.controller = function () {
   var ctrl = this;
 
 
-}
+};
 
 Layout.view = function (ctrl, options) {
   return m('.Layout', [
     m('h2', 'hello layout')
-  ])
-}
+  ]);
+};
 
-var subViews = function(ctrl) {
-  return m('')
-}
+
+
+
+var alertSubView = function(ctrl) {
+  return m("div", {class: "alert"}, [
+
+    m("button", {type: "button", class: "close"})
+
+  ]);
+};

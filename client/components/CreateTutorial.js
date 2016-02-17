a40812a7f120b8c3f944b0156a65e4f470bb5891
Grant Redfearn <grantredfearn@gmail.com>
2016-02-17 17:34:33 -0600

    fixed nav to show signout button

--- client/components/CreateTutorial.js
+++ client/components/CreateTutorial.js
@@ -27,21 +27,21 @@ var createTemplate = function() {
         m('legend', 'Tutorial Information'),
         m('form', 'Title: ', { type: 'text' }, [
           m('br'),
-          m('input', { type: 'text', placeholder: 'Enter Title', style: 'width: 45%' })
+          m('input', { type: 'text', placeholder: 'Enter Title', style: 'width: 55%' })
         ]),
         m('form', 'Description: ', { type: 'text' }, [
           m('br'),
-          m('input', { type: 'text', placeholder: 'Enter Description', style: 'width: 45%' })
+          m('input', { type: 'text', placeholder: 'Enter Description', style: 'width: 55%' })
         ])
       ]),
     ]),
     m('br'),
     m('div', [
-        m('fieldset', { style: 'margin-right: 35%;'}, [
+        m('fieldset', { style: 'margin-right: 33%;'}, [
           m('legend', 'Step Information'),
           m('form', 'Description:', { type: 'text' }, [
             m('br'),
-            m('input', { type: 'text', placeholder: 'Give a short description of step', style: 'width: 35%' })
+            m('input', { type: 'text', placeholder: 'Give a short description of step', style: 'width: 54%' })
           ]),
 
           m('form', 'Step: ', { type: 'text' }, [

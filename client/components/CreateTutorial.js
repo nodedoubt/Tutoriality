e408195078b5fcd84df31b735048f4e556f6b4ee
Grant Redfearn <grantredfearn@gmail.com>
2016-02-17 17:15:16 -0600

    basic create tutorial page, still needs button and some styles

--- client/components/CreateTutorial.js
+++ client/components/CreateTutorial.js
@@ -9,8 +9,39 @@ CreateTutorial.controller = function () {
 
 CreateTutorial.view = function (ctrl, options) {
   return m('.CreateTutorial', [
-    m('h2', 'hello create')
-  ])
+
+    m('h2', 'Create Tutorial'),
+
+    m('div', [
+      m('fieldset', { style:'margin-right: 35%' }, [
+        m('legend', 'Tutorial Information'),
+        m('form', 'Title: ', { type: 'text' }, [
+          m('br'),
+          m('input', { type: 'text', placeholder: 'Enter Title', style: 'width: 45%' })
+        ]),
+        m('form', 'Description: ', { type: 'text' }, [
+          m('br'),
+          m('input', { type: 'text', placeholder: 'Enter Description', style: 'width: 45%' })
+        ])
+      ]),
+    ]),
+    m('br'),
+    m('div', [
+        m('fieldset', { style: 'margin-right: 35%;'}, [
+          m('legend', 'Step Information'),
+          m('form', 'Description:', { type: 'text' }, [
+            m('br'),
+            m('input', { type: 'text', placeholder: 'Give a short description of step', style: 'width: 35%' })
+          ]),
+
+          m('form', 'Step: ', { type: 'text' }, [
+            m('br'),
+            m('textarea.form-control', { rows:'3', type:'text', placeholder:'Step it out!', style: 'width:75%; height:175px ' }),
+
+            ])
+          ])
+        ])
+    ])
 }
 
 var subViews = function(ctrl) {

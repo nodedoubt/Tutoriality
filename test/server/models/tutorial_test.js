d6fde0159586cd7386b4bdadbb4ecb0fb5184e13
Clay Branch <clay.branch@gmail.com>
2016-02-17 15:09:08 -0700

    added frame for client model tests

---
+++ test/server/models/tutorial_test.js
@@ -0,0 +1,8 @@
+require(TEST_HELPER) // <--- This must be at the top of every test file.
+
+var request = require('supertest')
+var Tutorial = require(__client + '/models/tutorial.js');
+
+describe("The Tutorial Client Model", function() {
+	
+});

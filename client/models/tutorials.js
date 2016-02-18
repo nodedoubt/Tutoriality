24dbb01704dfd8133e0dc6b0bc85335abdad7d2e
Clay Branch <clay.branch@gmail.com>
2016-02-17 17:01:51 -0700

    added fetch method to tutorial model

--- client/models/tutorials.js
+++ client/models/tutorials.js
@@ -5,3 +5,7 @@ var Tutorial = module.exports;
 Tutorial.fetchAll = function() {
 	return m.request({method : 'GET', url : '/api/tutorials'});
 }
+
+Tutorial.fetchByID = function(id) {
+	return m.request({method : 'GET', url : '/api/tutorials' + id});
+}

4e675d837ce49f2f020a305cc0ce855ec370073c
Clay Branch <clay.branch@gmail.com>
2016-02-17 17:02:10 -0700

    added the ability to prepopulate data to the database

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,11 +1,29 @@
 var pmongo = require('promised-mongo');
 var dbName = process.env.NODE_ENV === 'test' ? 'test' : 'local';
 var db = pmongo(dbName);
+var _ = require('underscore');
 db.deleteEverything = function () {
   return Promise.all([
     db.collection('tutorials').remove(),
   ])
 }
+
+db.loadFixtures = function() {
+	// Get the fixture from a file, this just an object
+	// where the key is the collection name and the
+	// value is an array of records to be inserted
+	var fixtures = require('./fixtures.js');
+	_.each(fixtures, function(records, collectionName){
+		db.collection(collectionName).find().then(function(items){
+			// only insert if the collection is empty
+			if(items.length === 0) {
+				db.collection(collectionName).insert(records);
+			}
+		})
+	});
+}
+
+db.loadFixtures();
 
 module.exports = db;
 

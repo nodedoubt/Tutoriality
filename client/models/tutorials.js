dc181f4dbf81d1debea618c2351bcff3ff88570b
Clay Branch <clay.branch@gmail.com>
2016-02-17 17:14:15 -0700

    set up basic tutorial model

--- client/models/tutorials.js
+++ client/models/tutorials.js
@@ -9,3 +9,7 @@ Tutorial.fetchAll = function() {
 Tutorial.fetchByID = function(id) {
 	return m.request({method : 'GET', url : '/api/tutorials' + id});
 }
+
+Tutorial.create = function(tutorial) {
+	return m.request({method : 'POST', url : '/api/tutorials', data : tutorial});
+}

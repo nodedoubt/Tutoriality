837dedf43387ec16badf29ad876e8207adfc75b4
Veronica Hernandez <vlhernandez@gmail.com>
2016-02-22 10:51:00 -0600

    comment out fake data

--- client/components/TutorialList.js
+++ client/components/TutorialList.js
@@ -58,46 +58,46 @@ List.view = function (ctrl, options) {
 };
 
 
-List.tutorials =[
-    { id: 0,
-      title: 'How to wash a dog',
-      description: 'A tutorial on how to wash a muddy pup',
-      steps: [
-        {
-          Title: 'Gather all dog washing supplies',
-          Description: 'Get dog shampoo, a basin or tub to wash the dog in and warm water'
-        },
-        {
-          Title: 'Wrangle dog into water',
-          Description: 'Use hotdogs or your speed to capture the dog and put it in the basin of water'
-        }
-      ]
-    },
-    { id: 1,
-      title: 'How to put on shoes',
-      description: 'A tutorial on how to put your shoes on',
-      steps: [
-          {
-            Title: 'Get two matching shoes',
-            Description: 'Find both matching shoes that you want to wear'
-          },
-          {
-            Title: 'Put shoes on',
-            Description: 'Make sure laces are untied and slip foot into shoe.'
-          }
-        ]
-    },    { id: 2,
-          title: 'How to use a phone',
-          description: 'A tutorial on how use a phone',
-          steps: [
-              {
-                Title: 'Dial a phone number',
-                Description: 'Use the keypad to dial someone\'s phone number'
-              },
-              {
-                Title: 'Say Hello',
-                Description: 'When that person answers, say hello. Otherwise, leave a message'
-              }
-            ]
-        }
-];
+// List.tutorials =[
+//     { id: 0,
+//       title: 'How to wash a dog',
+//       description: 'A tutorial on how to wash a muddy pup',
+//       steps: [
+//         {
+//           Title: 'Gather all dog washing supplies',
+//           Description: 'Get dog shampoo, a basin or tub to wash the dog in and warm water'
+//         },
+//         {
+//           Title: 'Wrangle dog into water',
+//           Description: 'Use hotdogs or your speed to capture the dog and put it in the basin of water'
+//         }
+//       ]
+//     },
+//     { id: 1,
+//       title: 'How to put on shoes',
+//       description: 'A tutorial on how to put your shoes on',
+//       steps: [
+//           {
+//             Title: 'Get two matching shoes',
+//             Description: 'Find both matching shoes that you want to wear'
+//           },
+//           {
+//             Title: 'Put shoes on',
+//             Description: 'Make sure laces are untied and slip foot into shoe.'
+//           }
+//         ]
+//     },    { id: 2,
+//           title: 'How to use a phone',
+//           description: 'A tutorial on how use a phone',
+//           steps: [
+//               {
+//                 Title: 'Dial a phone number',
+//                 Description: 'Use the keypad to dial someone\'s phone number'
+//               },
+//               {
+//                 Title: 'Say Hello',
+//                 Description: 'When that person answers, say hello. Otherwise, leave a message'
+//               }
+//             ]
+//         }
+// ];

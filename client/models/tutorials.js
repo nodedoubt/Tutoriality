13174ee53f80e2cca9c98c25340a7dc20e59b755
Grant Redfearn <grantredfearn@gmail.com>
2016-02-22 17:13:08 -0600

    commented models/tutorials

--- client/models/tutorials.js
+++ client/models/tutorials.js
@@ -4,22 +4,23 @@ var _ = require('underscore');
 var User = require('./users');
 var Tutorial = module.exports;
 
+// fetches all tutorials from the api
 Tutorial.fetchAll = function() {
 	return m.request({method : 'GET', url : '/api/tutorials'});
 }
-
+//fetches a tutorial by unique id created by database
 Tutorial.fetchByID = function(id) {
 	return m.request({method : 'GET', url : '/api/tutorials/' + id});
 }
-
+//creates a new database entry for a given tutorial
 Tutorial.create = function(tutorial) {
 	return m.request({method : 'POST', url : '/api/tutorials', data : tutorial});
 }
-
+//updates a tutorial by the id given to it
 Tutorial.updateByID = function(id, tutorial) {
   return m.request({method : 'PUT', url : '/api/tutorials/' + id, data : tutorial});
 }
-
+//Tutorial object view model creator
 Tutorial.tutorialVM = function () {
   return {
     title: '',
@@ -28,7 +29,7 @@ Tutorial.tutorialVM = function () {
     created_by : User.getID(),
   }
 }
-
+//Step object view model creator
 Tutorial.stepVM = function () {
   return {
     title: '',

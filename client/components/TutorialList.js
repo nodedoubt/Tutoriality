a7e4fe58eecbf37aee40ded803808ba180325fcb
Veronica Hernandez <vlhernandez@gmail.com>
2016-02-22 16:57:45 -0600

    update CSS, clean out leftover comments in TutorialList

--- client/components/TutorialList.js
+++ client/components/TutorialList.js
@@ -2,9 +2,9 @@ var m              = require('mithril');
 var _              = require('underscore');
 var Accordian      = require('./Accordion.js');
 var Tutorial       = require('../models/tutorials.js')
-var List           = module.exports;
 var mainLayout     = require('../layouts/main');
 var Read           = require('./ReadTutorial.js');
+var List           = module.exports;
 
 
 List.controller = function () {
@@ -19,16 +19,6 @@ List.controller = function () {
         };
       });
   });
-
-  // ctrl.tutorials = _.map(List.tutorials, function(tutorial) {
-  //     return {
-  //       id: tutorial.id,
-  //       title: tutorial.title,
-  //       content: m('.tutorial-content', tutorial.description)
-  //     };
-  //   });
-  //
-
 };
 
 List.view = function (ctrl, options) {
@@ -42,10 +32,10 @@ List.view = function (ctrl, options) {
       ctrl.tutorials.map(function(tutorial) {
         return m('div.panel.panel-default', [
           m('div.panel-heading', [
-            m('h3.panel-title.list-link',  { "data-id": tutorial.id, onclick: function(e){
-              var id = e.target.getAttribute('data-id');
-              m.route('/read/' + id)
-            }.bind(ctrl)}, tutorial.title)
+            m('h3.panel-title.list-link', {onclick: function(e){
+              // grab tutorial id and pass to read using variadic route
+              m.route('/read/' + tutorial.id)
+            }}, tutorial.title)
           ])
         ],
         m('div.panel-body', tutorial.content.children[0]));
@@ -56,48 +46,3 @@ List.view = function (ctrl, options) {
 
   return mainLayout(view);
 };
-
-
-// List.tutorials =[
-//     { id: 0,
-//       title: 'How to wash a dog',
-//       description: 'A tutorial on how to wash a muddy pup',
-//       steps: [
-//         {
-//           Title: 'Gather all dog washing supplies',
-//           Description: 'Get dog shampoo, a basin or tub to wash the dog in and warm water'
-//         },
-//         {
-//           Title: 'Wrangle dog into water',
-//           Description: 'Use hotdogs or your speed to capture the dog and put it in the basin of water'
-//         }
-//       ]
-//     },
-//     { id: 1,
-//       title: 'How to put on shoes',
-//       description: 'A tutorial on how to put your shoes on',
-//       steps: [
-//           {
-//             Title: 'Get two matching shoes',
-//             Description: 'Find both matching shoes that you want to wear'
-//           },
-//           {
-//             Title: 'Put shoes on',
-//             Description: 'Make sure laces are untied and slip foot into shoe.'
-//           }
-//         ]
-//     },    { id: 2,
-//           title: 'How to use a phone',
-//           description: 'A tutorial on how use a phone',
-//           steps: [
-//               {
-//                 Title: 'Dial a phone number',
-//                 Description: 'Use the keypad to dial someone\'s phone number'
-//               },
-//               {
-//                 Title: 'Say Hello',
-//                 Description: 'When that person answers, say hello. Otherwise, leave a message'
-//               }
-//             ]
-//         }
-// ];

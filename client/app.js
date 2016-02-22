8cbc3c5e96751c353a4ee0badd7a6fb129cb7a08
Grant Redfearn <grantredfearn@gmail.com>
2016-02-22 17:15:58 -0600

    commented client/app.js

--- client/app.js
+++ client/app.js
@@ -8,9 +8,12 @@ window.App = {}
 //
 // Client-side routing
 //
+
+//sets the type of route to hash. e.i. localhost:4000/#/create
 m.route.mode = "hash"
 // m.route.mode = 'pathname'
 
+//defines all of the various routes
 m.route(document.getElementById('app'), '/', {
   '/'   : require('./components/TutorialList'),
   '/list'   : require('./components/TutorialList'),

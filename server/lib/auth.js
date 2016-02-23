d401cb89e2dcfd5d33af413f2503ff0aaf4e3863
Justin Seiter <justinseiter@gmail.com>
2016-02-23 16:42:49 -0600

    Update auth and set config for mongo prod

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -1,11 +1,13 @@
 var OAuth = require('oauthio');
-OAuth.initialize(process.env.OAUTHIO_PUBLIC_KEY);
+// var credentials = require('./config.js').credentials.oauth;
+OAuth.initialize("2dtinySKQP0jVXxfP5_O7dwZq_M", "Nx8zeUlnczRitqwuam9HhzxjV9U");
 OAuth.confirmLogin = function(request, response, next) {
-	OAuth.auth('github', request.session).then(function(){
-		next();
-	}).catch(function(){
-		response.redirect('/api/sign-in');
-	});
+  var credentials = request.getCredentials();
+  console.log('here are the creds', credentials)
+  OAuth.auth('github', request.session).then(function(){
+    next();
+  }).catch(function(){
+    response.redirect('/api/sign-in');
+  });
 }
 module.exports = OAuth;
-
